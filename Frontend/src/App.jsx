f3f831b15d472f8a9429c00239b4cdf89a76f799
Vishal Mahajan <vism06@gmail.com>
2024-08-15 14:45:45 +0530

    Changed Path to solve deployment issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar/navbar.jsx'
+import Navbar from '@/components/Navbar/Navbar.jsx'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home.jsx'
-import Cart from './pages/Cart/Cart.jsx'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
-import Footer from './components/Footer/Footer.jsx'
+import Home from '@/pages/Home/Home.jsx'
+import Cart from '@/pages/Cart/Cart.jsx'
+import PlaceOrder from '@/pages/PlaceOrder/PlaceOrder.jsx'
+import Footer from '@/components/Footer/Footer.jsx'
 
 const App = () => {
   return (

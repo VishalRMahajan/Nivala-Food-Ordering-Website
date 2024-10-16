f38fe1557a3c59a61f62443731e6443cfe7236ce
Vishal Mahajan <vism06@gmail.com>
2024-10-16 22:48:06 +0530

    Solved small error causing backend deployment error

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import { placeOrder } from "../controllers/OrderController.js";
+import { placeOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 

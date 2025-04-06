e4d7e2c03e919849cc2ed3403a988d0588e676ff
Vishal Mahajan <vism06@gmail.com>
2025-04-06 22:52:09 +0530

    Solved CORS..

--- Backend/server.js
+++ Backend/server.js
@@ -1,46 +1,43 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
+import express from "express";
+import cors from "cors";
+import path from "path";
 
-import { connectDB } from './config/db.js';
-import foodRouter from './routes/foodRoute.js';
-import userRouter from './routes/userRoute.js';
-import 'dotenv/config'
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
-import PromoRouter from './routes/promoRoute.js';
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import PromoRouter from "./routes/promoRoute.js";
 
 //app config
 const app = express();
 const port = 4000;
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({ origin: "https://nivala.vishalrmahajan.in" }));
 
 //DB config
 connectDB();
 
 //EndPoints
-app.use("/api/food",foodRouter)
-app.use("/api/auth",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
-app.use("/api/promo",PromoRouter)
+app.use("/api/food", foodRouter);
+app.use("/api/auth", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/promo", PromoRouter);
 const __dirname = path.resolve();
-app.use("/images",express.static(path.join(__dirname, 'uploads')))
+app.use("/images", express.static(path.join(__dirname, "uploads")));
 
-
-app.get("/",(req,res)=>{
-    res.status(200).json({message:"Backend Server is Awake"})
-})
-
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Backend Server is Awake" });
+});
 
 //listen with callback function
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 //
-

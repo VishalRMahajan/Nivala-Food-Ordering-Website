4a3141c61b87bd61c3a8e2860390bcf034f933b5
Vishal Mahajan <vism06@gmail.com>
2025-04-06 22:47:28 +0530

    Added service worker

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import StoreContextProvider from './context/StoreContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./context/StoreContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <StoreContextProvider >
+    <StoreContextProvider>
       <App />
     </StoreContextProvider>
   </BrowserRouter>
-
-
-)
+);
